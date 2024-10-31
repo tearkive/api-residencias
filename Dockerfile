6edc8618b952a8aeccecf428f2ca84574fe3581c
DiegoBecerra613 <diegoantoniobecerrapicon@gmail.com>
2024-10-30 21:42:07 -0600

    Se cambio el puerto

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ RUN npm install -g nodemon
 COPY package*.json ./
 RUN npm install
 COPY . /app
-EXPOSE 3000
+EXPOSE 3001
 CMD ["node","app.js"]
--- app.js
+++ app.js
@@ -60,8 +60,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, function(){
-  console.log('Listening on port 3000');
+app.listen(3001, function(){
+  console.log('Listening on port 3001');
 });
 
 module.exports = app;
